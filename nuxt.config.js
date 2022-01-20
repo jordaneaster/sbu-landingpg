4fdbfa013017015312558218bcff4529b13d4999
jordaneaster <jordane@am-collab.com>
2022-01-20 15:35:34 -0500

    updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'sbu-landing',
+    title: 'Space Babiez NFTZ 2022',
     htmlAttrs: {
       lang: 'en',
     },
@@ -18,7 +18,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/logo1.png' },
       {
         rel: 'stylesheet',
 
